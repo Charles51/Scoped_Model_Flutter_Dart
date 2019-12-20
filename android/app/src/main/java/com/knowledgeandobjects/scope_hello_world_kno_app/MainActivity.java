c1040455446ffce7776c9243d6341ef4ce18275f
Charles Dow <charles.dow@knowledgeandobjects.com>
2019-12-20 08:10:32 -0500

    First push to git

---
+++ android/app/src/main/java/com/knowledgeandobjects/scope_hello_world_kno_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.knowledgeandobjects.scope_hello_world_kno_app;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
